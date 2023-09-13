2f3819e8b7d637161f10acac1fa34d5d67b945c6
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-13 17:42:09 +0530

    Creative borders and fills using ImagePaint

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,46 +7,14 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffset = -20.0
-    var petalWidth = 100.0
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-            
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-            
-            let rotatedPetal = originalPetal.applying(position)
-            
-            path.addPath(rotatedPetal)
-        }
-        
-        return path
-    }
-}
-
 struct ContentView: View {
     @State private var petalOffset = -20.0
     @State private var petalWidht = 100.0
 
     var body: some View {
-        VStack {
-            Flower(petalOffset: petalOffset, petalWidth: petalWidht)
-                .fill(.red, style: FillStyle(eoFill: true))
-//                .stroke(.red, lineWidth: 1)
-            
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            Text("Width")
-            Slider(value: $petalWidht, in: 0...100)
-                .padding(.horizontal)
-        }
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.25), scale: 0.3), lineWidth: 50)
+            .frame(height: 500)
     }
 }
 
