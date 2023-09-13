49314e1adf02698ed73f83b87101b8349dba2894
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-13 17:29:57 +0530

    Transforming shapes using CGAffineTransform and even-odd fills

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,53 +7,46 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
+struct Flower: Shape {
+    var petalOffset = -20.0
+    var petalWidth = 100.0
+    
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+        }
         
         return path
-    }
-}
-
-struct Arc: InsettableShape {
-    let startAngle: Angle
-    let endAngle: Angle
-    let cloclWise: Bool
-    var insetAmount = 0.0
-    
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjustment = Angle.degrees(90)
-        let modifiedStartAngle = startAngle - rotationAdjustment
-        let modifiedEndAngle = endAngle - rotationAdjustment
-        
-        var path = Path()
-        
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStartAngle, endAngle: modifiedEndAngle, clockwise: !cloclWise)
-        
-        return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
     }
 }
 
 struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidht = 100.0
+
     var body: some View {
-//        Triangle()
-//            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-//            .frame(width: 300, height: 300)
-        
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), cloclWise: true)
-            .strokeBorder(.blue, lineWidth: 50)
-        
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidht)
+                .fill(.red, style: FillStyle(eoFill: true))
+//                .stroke(.red, lineWidth: 1)
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidht, in: 0...100)
+                .padding(.horizontal)
+        }
     }
 }
 
