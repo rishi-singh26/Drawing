9fe4def78fa0153fbed0e1722cac1c81a2ab04ce
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-13 16:32:49 +0530

    Paths vs shapes in SwiftUI

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,15 +7,46 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: Shape {
+    let startAngle: Angle
+    let endAngle: Angle
+    let cloclWise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStartAngle = startAngle - rotationAdjustment
+        let modifiedEndAngle = endAngle - rotationAdjustment
+        
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStartAngle, endAngle: modifiedEndAngle, clockwise: !cloclWise)
+        
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-        }
-        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//        Triangle()
+//            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//            .frame(width: 300, height: 300)
+        
+        Arc(startAngle: .degrees(0), endAngle: .degrees(110), cloclWise: true)
+            .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            .frame(width: 300, height: 300)
     }
 }
 
