153274f739218d39a1180e1a0bf86782263c7b78
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-13 11:36:44 +0530

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Rishi Singh on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
